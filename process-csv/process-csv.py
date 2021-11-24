95bcec9ec704e44cd7054612c221a6964005b418
Roger Holden <r.holden@chi.ac.uk>
2021-11-24 19:29:28 +0000

    cvs to sql

---
+++ process-csv/process-csv.py
@@ -0,0 +1,41 @@
+import csv
+import sys
+table = 'postcodes'
+inputFile = '/home/roger/Downloads/postcodes.csv'
+
+# lsoa_name,LSOA
+
+sql = "CREATE TABLE `"+ table +"` \n (`postcode` varchar(12) DEFAULT NULL,\n  `postcode2` varchar(12) DEFAULT NULL,\n `lsoa_name` varchar(255) DEFAULT NULL, \n`LSOA` varchar(12) DEFAULT NULL )\n ENGINE=InnoDB \nDEFAULT CHARSET=utf8;\n\n"
+
+rowCount = 1
+
+with open(inputFile, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    totalrows = len(open(inputFile).readlines())
+    print( totalrows)
+
+    sql += "INSERT INTO `"+ table+"` (`postcode`, `postcode2`,`lsoa_name`, `LSOA`) VALUES \n"
+     
+    # sys.exit()
+    
+    for row in reader:
+        lsoa_name = row["lsoa_name"].replace("'", "''")
+        lsoa_name = lsoa_name.replace(",", "::")
+
+        sql += " ('" + row["postcode"] + "' , '"+ row["postcode2"]+ "',   '"+  lsoa_name+ "', '"+ row["LSOA"]+ "')"
+        rowCount +=1
+
+        if rowCount != totalrows:
+            sql += ","
+
+        sql += "\n"
+
+        # if rowCount == 10:
+        #     break
+
+
+
+f = open("postcodes.sql", "w")
+f.write(sql)
+f.close()
